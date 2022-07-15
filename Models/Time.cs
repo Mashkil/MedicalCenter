0e3f06c53aea86fe197503c189db4eff8cba6313
Mark _ <marik09292000@gmail.com>
2022-07-15 15:52:31 +0300

    сохранение проекта

---
+++ Models/Time.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MedicalCenter.Models
+{
+    internal class Time
+    {
+        public int Id { get; set; }
+        public DateTime time { get; set; }
+
+    }
+}
